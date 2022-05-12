745fb045b1b6fd6da57537709538e885e421a4ca
Pieter <pieter_delamar@hotmail.com>
2022-05-12 15:43:22 +0200

    Medewerker en Manager als subclasses van User gevuld met constructor en variabelen.

--- src/main/java/com/example/carbonbattles/Manager.java
+++ src/main/java/com/example/carbonbattles/Manager.java
@@ -1,4 +1,10 @@
 package com.example.carbonbattles;
 
 public class Manager extends User {
+
+    public Manager(String naam, String gebruikersnaam, String wachtwoord, boolean isAdmin, int aantalPunten) {
+        super(naam, gebruikersnaam, wachtwoord, isAdmin, aantalPunten);
+    }
+
+
 }
--- src/main/java/com/example/carbonbattles/Medewerker.java
+++ src/main/java/com/example/carbonbattles/Medewerker.java
@@ -1,4 +1,10 @@
 package com.example.carbonbattles;
 
-public class Medewerker extends User{
+public class Medewerker extends User {
+
+    public Medewerker(String naam, String gebruikersnaam, String wachtwoord, boolean isAdmin, int aantalPunten) {
+        super(naam, gebruikersnaam, wachtwoord, isAdmin, aantalPunten);
+    }
+
+
 }
