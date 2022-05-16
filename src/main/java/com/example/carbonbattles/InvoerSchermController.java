10f5640e177c781546780861f4b68b0fe8fa2bb0
Konrad Zwolinski <zwolinskikonrad@gmail.com>
2022-05-16 19:08:19 +0200

    Button toegevoegd om terug te gaan naar menu, ook invoer veld voor datum geimplementeerd en tests aangepast adhv veranderingen in code.

--- src/main/java/com/example/carbonbattles/InvoerSchermController.java
+++ src/main/java/com/example/carbonbattles/InvoerSchermController.java
@@ -3,11 +3,17 @@ package com.example.carbonbattles;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -19,7 +25,6 @@ public class InvoerSchermController {
     @FXML
     private ChoiceBox<Voertuig> boxKeuzes;
 
-
     @FXML
     private CheckBox elektrischCheckbox;
 
@@ -27,32 +32,43 @@ public class InvoerSchermController {
     private TextField kilometerVeld;
 
     @FXML
-    void maakVoertuigElektrisch(ActionEvent event) {
+    private TextField datumVeld;
+
+    @FXML
+    void gaNaarMenuScherm(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MedewerkerMenu.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("CarbonBattles");
+        stage.setResizable(false);
+        stage.show();
     }
 
     @FXML
     void verzendGegevens(ActionEvent event) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        LocalDateTime now = LocalDateTime.now();
-
         if (boxKeuzes.getSelectionModel().isEmpty()) {
             System.out.println("Niks geselecteerd");
         } else {
-            if (kilometerVeld.getText().isEmpty()) {
-                System.out.println("Niks ingevoerd");
+            if (checkInputWaarden()) {
+                System.out.println("ERROR");
             } else {
 
                 Integer kilometers = Integer.parseInt(kilometerVeld.getText());
                 Voertuig voertuig = boxKeuzes.getSelectionModel().getSelectedItem();
 
                 //Maakt een Rit aan en geeft dit mee aan een gebruiker, moet nog een oplossing voor komen hoe je ingelogde user hier plaatst!!!
-                carbonbattle.getUsers().get(0).createARit(kilometers, voertuig, elektrischCheckbox.isSelected(), dtf.format(now));
+                carbonbattle.getUsers().get(0).createARit(kilometers, voertuig, elektrischCheckbox.isSelected(), datumVeld.getText());
 
                 //Test printen van het aantal punten van een rit
-                System.out.println(carbonbattle.getUsers().get(0).getNaam() + " heeft "+
-                                    carbonbattle.getUsers().get(0).getAantalPunten() + " punten ontvangen op " +
-                                    carbonbattle.getUsers().get(0).getRitten().get(0).getDatum());
+                System.out.println(carbonbattle.getUsers().get(0).getNaam() + " heeft " +
+                        carbonbattle.getUsers().get(0).getAantalPunten() + " punten ontvangen op " +
+                        carbonbattle.getUsers().get(0).getRitten().get(0).getDatum());
             }
         }
+    }
+
+    private boolean checkInputWaarden() {
+        return kilometerVeld.getText().isEmpty() || kilometerVeld.getText().equals("0") || datumVeld.getText().isEmpty() || kilometerVeld.getText().contains("-");
     }
 }
