822eb868fec9adf2c045ee1d136712f87f8b666a
MrWizzxrd <adamitos10@gmail.com>
2022-05-18 15:48:09 +0200

    Logo toegevoegd en wat bugs gefixt

--- src/main/java/com/example/carbonbattles/AuthenticateController.java
+++ src/main/java/com/example/carbonbattles/AuthenticateController.java
@@ -39,29 +39,24 @@ public class AuthenticateController extends CarbonBattles {
         for (User u : CarbonBattles.getUsers()) {
             if (u.getGebruikersNaam().equals(usernameTextField.getText().toString())) {
                 if (u.getWachtwoord().equals(passwordField.getText().toString())) {
+                    Parent root;
                     if (u.isAdmin()) {
-                        Parent root = FXMLLoader.load(getClass().getResource("ManagerMenu.fxml"));
-                        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                        scene = new Scene(root);
-                        stage.setScene(scene);
-                        stage.setTitle("CarbonBattles");
-                        stage.setResizable(false);
-                        stage.show();
+                        root = FXMLLoader.load(getClass().getResource("ManagerMenu.fxml"));
 
                     } else {
-                        Parent root = FXMLLoader.load(getClass().getResource("MedewerkerMenu.fxml"));
-                        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                        scene = new Scene(root);
-                        stage.setScene(scene);
-                        stage.setTitle("CarbonBattles");
-                        stage.setResizable(false);
-                        stage.show();
+                        root = FXMLLoader.load(getClass().getResource("MedewerkerMenu.fxml"));
                     }
+                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.setTitle("CarbonBattles");
+                    stage.setResizable(false);
+                    stage.show();
                 } else {
                     loginMessageLabel.setText("Incorrect credentials!");
                 }
             }
-            else if (usernameTextField.getText().isBlank() == true && passwordField.getText().isBlank() == true){
+            else if (usernameTextField.getText().isBlank() && passwordField.getText().isBlank()){
                 loginMessageLabel.setText("Please enter your Username and Password.");
             }
             else {
