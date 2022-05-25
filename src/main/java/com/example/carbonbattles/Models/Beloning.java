31a388fb6444996834fa276e6c079e757afdfbcd
BuildTools <unconfigured@null.spigotmc.org>
2022-05-25 20:24:46 +0200

    Branch aangemaakt

--- src/main/java/com/example/carbonbattles/Models/Beloning.java
+++ src/main/java/com/example/carbonbattles/Models/Beloning.java
@@ -5,25 +5,14 @@ public class Beloning {
     private String Beloning;
     private Integer nettoPuntenVerandering;
     private String datum;
+
     public Beloning(String naam, String beloning, Integer nettoPuntenVerandering, String datum) {
         this.naam = naam;
         Beloning = beloning;
         this.nettoPuntenVerandering = nettoPuntenVerandering;
         this.datum = datum;
     }
-
-    public String getBeloning() {
-        return Beloning;
-    }
-
-    public String getDatum() {
-        return datum;
-    }
-
-    public String getNaam() {
-        return naam;
-    }
-
+    
     @Override
     public String toString() {
         return "Beloning gekregen op: " + datum + "           " + "- " + nettoPuntenVerandering.toString() + " Punten";
