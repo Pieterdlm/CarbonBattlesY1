d8d27161341fb86d113cb6845eee40771b4f484a
MrWizzxrd <adamitos10@gmail.com>
2022-05-30 11:10:54 +0200

    Linked FXML'S and added aantal kilometer achievment

--- src/main/java/com/example/carbonbattles/MenuController.java
+++ src/main/java/com/example/carbonbattles/MenuController.java
@@ -81,4 +81,25 @@ public class MenuController {
         stage.show();
 
     }
+    public void inzichtSwitchOnAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("InzichtPortaal-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("CarbonBattles");
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void invoerMedewerkerSwitchOnAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("BeherenUsers-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("CarbonBattles");
+        stage.setResizable(false);
+        stage.show();
+    }
+
+
 }
